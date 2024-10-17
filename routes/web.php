92e67ab37fc46bc997a6682c0e7b08d239e17acb
megaeonise <54655406+megaeonise@users.noreply.github.com>
2024-10-18 03:04:48 +0600

    added route and controller file for notes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GachaController;
+use App\Http\Controllers\NoteController;
 
 Route::get('/', [DashboardController::class, 'index']) ;
 
 Route::get('/roll', [GachaController::class, 'index']) ;
+
+Route::get('/note', [NoteController::class, 'index']) ;
