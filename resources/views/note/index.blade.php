9bd529dc17f1af0f76ebf716ebb1927a4c36e143
megaeonise <54655406+megaeonise@users.noreply.github.com>
2024-10-22 23:00:30 +0600

    made the index look better

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,18 +1,27 @@
 <x-layout>
    this is index
    <div class="task-container">
-    <a href="#" class = "new-task-btn">
+    <a href="{{route('note.create')}}" class = "new-task-btn">
         Click to add tasks
     </a>
     <div class="tasks">
         @foreach ($note as $task)
         <div class="task">
+            <div class="task-start">
+                <h3>Start: </h3>
+                {{ $task->Start}}
+            </div>
             <div class="task-body">
+                <h3>Task: </h3>
                 {{ Str::words($task->Task, 100)}}
             </div>
+            <div class="task-due">
+                <h3>Due: </h3>
+                {{ $task->Due}}
+            </div>
             <div class="task-buttons">
-                <a href="#" class="task-viewbtn">View</a>
-                <a href="#" class="task-editbtn">Edit</a>
+                <a href="{{route('note.create')}}" class="task-viewbtn">View</a>
+                <a href="{{route('note.create')}}" class="task-editbtn">Edit</a>
                 <button class="task-delbtn">Delete</button>
             </div>
         </div>
